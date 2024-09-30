002195b6b4c0882a26909f33d0ec04c86deda71d
Anastasia Rodionova <argali96@gmail.com>
2024-09-30 19:35:12 +0200

    Optimize rendering

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -3,23 +3,8 @@ import { Effect } from "effect"
 import { Buffer } from "node:buffer"
 import sharp from "sharp"
 
-export const resolveImage = (image: string) =>
-  Effect.gen(function* () {
-    const httpClient = yield* HttpClient.HttpClient
-
-    const req = HttpClientRequest.get(image)
-
-    const resp = yield* httpClient.execute(req)
-
-    if (resp.status === 200) {
-      return image
-    }
-
-    return null
-  })
-
 export const resolveTokenIcon = (address: string) =>
-  resolveImage(
+  resolveToJpeg(
     `https://tokens-data.1inch.io/images/${address.toLowerCase()}.png`,
   )
 
